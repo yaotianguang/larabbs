82fd78ba9cb58d30f485d53d466d2f4e8c2723df
yaotianguang <3152537519@qq.com>
2022-07-03 15:23:25 +0800

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -8,5 +8,21 @@ class Topic extends Model
 {
     use HasFactory;
     
-    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+    protected $fillable = [
+        'title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count',
+        'last_reply_user_id', 'order', 'excerpt', 'slug'
+    ];
+
+    // 一个话题属于一个分类
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // 一个话题属于一个作者
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }

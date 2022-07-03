18c825b5fc42af7dc60f6dac684a2c3f89d604cd
yaotianguang <3152537519@qq.com>
2022-07-03 09:49:03 +0800

    显示个人资料

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Auth;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        //authorize() 方法是表单验证自带的另一个功能 —— 权限验证，
+        //本课程中我们不会使用此功能，关于用户授权，我们将会在后面章节中使用更具扩展性的方案，此处我们 return true;
+        //return false;
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
+            'email' => 'required|email',
+            'introduction' => 'max:80',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.unique' => '用户名已被占用，请重新填写',
+            'name.regex' => '用户名只支持英文、数字、横杠和下划线。',
+            'name.between' => '用户名必须介于 3 - 25 个字符之间。',
+            'name.required' => '用户名不能为空。',
+        ];
+    }
+
+}

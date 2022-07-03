daffcb6b00165e03a6d13aafa064208d4023cdc2
yaotianguang <3152537519@qq.com>
2022-07-03 11:52:21 +0800

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,7 +9,11 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
@@ -17,15 +21,19 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
+
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
+
         $data = $request->all();
 
         if ($request->avatar) {
-            $result = $uploader->save($request->avatar, 'avatars', $user->id); //save(文件名, 文件夹名, 用户ID)
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 416); //save(文件名, 文件夹名, 用户ID, 最大宽度)
             if ($result) {
                 $data['avatar'] = $result['path'];
             }
